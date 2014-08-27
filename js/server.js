5bd1b51a9965378fb9c991ecc5015dfbc58e590c
The-Lore <therese.lorenz@fortitu.de>
2014-08-27 12:18:45 +0200

    Added statistics functions

--- js/server.js
+++ js/server.js
@@ -1,7 +1,16 @@
 const port=1337;
 
+var statisticFile = require('./statisticFile');
+var userCount;
+
 var express = require('express');
 var app=express();
+
+
+function increaseRequestCountByOne() {
+    userCount++;
+    console.log("User:" + userCount);
+}
 
 app.get('/', function(req,res){
     res.send('<a href ="http://localhost:1337/myname">http://localhost:1337/myname</a> ')
@@ -12,8 +21,17 @@ app.get('/statistics', function(req,res){
 });
 
 app.get('/:user', function(req,res){
+    increaseRequestCountByOne();
+    statisticFile.addUserNameToLog(req.param('user'));
     res.send("Hello " + req.param('user'));
 });
+
+
+statisticFile.readPreviousNumberOfUsers(function(number){
+    userCount = number;
+    console.log("we have changed user count " + userCount);
+})
+
 var server = app.listen(port,function(){
     console.log('Listening on port %d', server.address().port);
 });
