6e3402bde81122050aa88588c02afe3d99794544
angela <asindic@gmail.com>
2015-08-17 15:43:44 +0200

    port changed back to 1337

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     var answer =  util.helloWorld();
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(answer);
-}).listen(1338, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1338/');
+console.log('Server running at http://127.0.0.1:1337/');
 
