2b5cf83ff53334924743c010abf7b3ec9cccfce2
fhaidary <eng.haidary_farzaneh@yahoo.com>
2015-08-17 20:17:38 +0200

    Counter im Webbrowser

--- js/server.js
+++ js/server.js
@@ -1,17 +1,17 @@
 var http = require('http');
 var util = require('./util');
-var counter;
+var counter=0;
 
 http.createServer(function (req, res) {
     var answer =  util.helloWorld();
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(answer);
+    res.end(answer+' '+counter);
     counter = counter + 1;
     console.log(counter);
 
 
-}).listen(1337, '127.0.0.1');
-counter = 0;
+}).listen(1337, '127.0.0.1' );
+//counter = 0;
 console.log('Server running at http://127.0.0.1:1337/');
 
