686bd5c4520a4a41ffd96ddf2636da3a95c65fe3
angela <asindic@gmail.com>
2015-08-17 16:27:12 +0200

    counter added

--- js/server.js
+++ js/server.js
@@ -1,12 +1,17 @@
 var http = require('http');
 var util = require('./util');
+var counter;
 
 http.createServer(function (req, res) {
     var answer =  util.helloWorld();
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(answer);
-    console.log('answer to request');
-}).listen(1337, '127.0.0.1');
+    counter = counter + 1;
+    console.log(counter);
 
+
+}).listen(1337, '127.0.0.1');
+counter = 0;
 console.log('Server running at http://127.0.0.1:1337/');
 
