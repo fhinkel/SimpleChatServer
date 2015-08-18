b54f34a3c82ab76588b0858d65a25acdd17e9a34
fhaidary <eng.haidary_farzaneh@yahoo.com>
2015-08-18 20:39:55 +0200

    Corrected favicon and added list of users

--- js/Express/app.js
+++ js/Express/app.js
@@ -3,21 +3,29 @@ var favicon = require('serve-favicon');
 var app = express();
 var counterAdmin = 0;
 var counterUser = 0;
-
+var users = {}
 
 app.get('/', function (req, res) {
     res.send('Hello..schreibe deinen Namen hinter den Slash!');
 });
 
 app.get('/admin', function (req, res) {
-    res.send('Hello admin!\n admin: '+ counterAdmin++ + '\n user: ' + counterUser);
+    res.send('Hello admin!\n admin: '+ counterAdmin++ + '\n user: ' + counterUser+' '+JSON.stringify(users));
 });
 
 app.get('/user/:id', function (req, res) {
-    res.send('Hello !' + req.params.id);
+    var username = req.params.id;
+    res.send('Hello !' + username);
+    users[username] = true;
     counterUser++;
 });
-app.use(favicon( __dirname + '../../../Public/favicon.ico'));
+
+/* dirname=aktuelles Verzeichnis
+   ..=eine Ebene höher --> js
+   ..= nochmal -- > SimpleChatServer
+ */
+var file = __dirname + '/../../Public/favicon.ico';
+app.use(favicon(file));
 
 var server = app.listen(1338, function () {
     console.log('Example app listening at http://127.0.0.1:1338');
