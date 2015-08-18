1cb3b30e91d21992dba7ecbd848c8dc0affaf44d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-18 20:41:47 +0200

    REFACTOR Extract greeting callback into its own function

--- js/server.js
+++ js/server.js
@@ -3,12 +3,14 @@ var util = require('./util');
 var counter = 0;
 var port = 1337;
 
-http.createServer(function (req, res) {
+var greeting = function (req, res) {
     var answer = util.helloWorld();
     counter = counter + 1;
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(answer + '\nYou are user number ' + counter + '.');
     console.log('Current count: ' + counter);
-}).listen(port, '127.0.0.1');
+};
+
+http.createServer(greeting).listen(port, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:' + port + '/');
 
