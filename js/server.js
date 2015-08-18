db68227c3a02dcc059839592de9076abe52d3bfe
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-18 20:38:48 +0200

    FIX Increase counter before printing it

--- js/server.js
+++ js/server.js
@@ -5,9 +5,9 @@ var port = 1337;
 
 http.createServer(function (req, res) {
     var answer = util.helloWorld();
+    counter = counter + 1;
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(answer + '\nYou are user number ' + counter + '.');
-    counter = counter + 1;
     console.log('Current count: ' + counter);
 }).listen(port, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:' + port + '/');
