15df5b0aac5d0cbf58f7a920f33603b58d083a9f
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-18 20:38:23 +0200

    REFACTOR Improve messages about counter

--- js/server.js
+++ js/server.js
@@ -5,13 +5,10 @@ var port = 1337;
 
 http.createServer(function (req, res) {
     var answer = util.helloWorld();
-
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(answer + ' ' + counter);
+    res.end(answer + '\nYou are user number ' + counter + '.');
     counter = counter + 1;
-    console.log(counter);
-
-
+    console.log('Current count: ' + counter);
 }).listen(port, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:' + port + '/');
 
