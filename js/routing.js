8acdd06ba7d88a6f5743946d1e45e4d5ced50f0a
sara235 <rpg-freak@gmx.net>
2015-08-18 15:36:14 +0200

    UPDATED routing.js after code review, small fixes

--- js/routing.js
+++ js/routing.js
@@ -1,23 +1,32 @@
 var http = require('http');
 var counter = 0;
 var name;
+var responseMsg = "";
 http.createServer(function (req, res) {
     var str = req.url;
-    console.log(str);
+
     var pos = str.lastIndexOf("/");
     if(pos!=-1)
         name =  str.substring(pos+1);
-    if(name!="favicon.ico"){
-         counter++;
+
+    if (name == "favicon.ico") {
+        //TODO: add favicon
+        return
     }
-    if(name=="admin"){
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end("Total logged in "+counter);
+    if (name == "") name = "guest";
+
+    counter++;
+    responseMsg = "Hallo " + name + "!";
+
+    if (name == "admin") {
+        responseMsg += " Total logged in " + counter;
     }
-    else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Hallo " + name + "!");
-    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(responseMsg);
+
+    console.log(name + " logging in");
+
 }).listen(8080, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:8080/');
