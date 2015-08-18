231856de571d411013efbbef9a868d457d22fe63
nadjab <nadja.bogdanowa@gmail.com>
2015-08-18 11:11:06 +0200

    fav icon update

--- js/server.js
+++ js/server.js
@@ -1,16 +1,17 @@
 var http = require('http');
 var util = require('./util');
-var counter = 0;
-var lastUrl = '';
+var express =require('node_modules/express')
 
 http.createServer(function (req, res) {
-    var answer =  util.helloWorld();
+    var answer =  util.helloWorld() + (i+1);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    counter++;
-    console.log('Counter: ' + counter);
     res.end(answer);
+    i++;
+    console.log(i);
 }).listen(1337, '127.0.0.1');
+
+var i = 0;
 
 console.log('Server running at http://127.0.0.1:1337/');
 
+app.use('/favicon.ico', express.static('images/favicon.ico'));
