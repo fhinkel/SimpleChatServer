77cd7e9de6317657cabe421f22011ad78909351b
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-18 20:36:26 +0200

    REFACTOR Extract port into its own variable

--- js/server.js
+++ js/server.js
@@ -1,17 +1,17 @@
 var http = require('http');
 var util = require('./util');
-var counter=0;
+var counter = 0;
+var port = 1337;
 
 http.createServer(function (req, res) {
-    var answer =  util.helloWorld();
+    var answer = util.helloWorld();
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(answer+' '+counter);
+    res.end(answer + ' ' + counter);
     counter = counter + 1;
     console.log(counter);
 
 
-}).listen(1337, '127.0.0.1' );
-//counter = 0;
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(port, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
