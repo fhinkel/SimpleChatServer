0b7ce4bbd3d0d43a5e747aaed3cc2c214147f439
sara235 <rpg-freak@gmx.net>
2015-08-18 11:03:15 +0200

    CREATE branch

--- js/server.js
+++ js/server.js
@@ -1,9 +1,17 @@
 var http = require('http');
 var util = require('./util');
+var counter = 0;
+var lastUrl = '';
 
 http.createServer(function (req, res) {
     var answer =  util.helloWorld();
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    switch(req.url)
+    {
+    	lastUrl = req.url;
+    }
+    counter++;
+    console.log('Counter: ' + counter);
     res.end(answer);
 }).listen(1337, '127.0.0.1');
 
