83593cb20a74088eace03147d03c8ca68a2db58a
Antje <moeller@sitegeist.de>
2015-08-18 12:15:53 +0200

    Write out name from user and counter

--- js/Express/app.js
+++ js/Express/app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
+var counterAdmin = 0;
+var counterUser = 0;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+
+app.get('/admin', function (req, res) {
+    res.send('Hello admin!\n admin: '+ counterAdmin++ + '\n user: ' + counterUser);
+});
+
+app.get('/user/:id', function (req, res) {
+    res.send('Hello !' + req.params.id);
+    counterUser++;
 });
 
 var server = app.listen(1338, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
     console.log('Example app listening at http://127.0.0.1:1338', host, port);
 });
