ab2714b8a99c4da6e905368c1d0801e43d899aec
sara235 <rpg-freak@gmx.net>
2015-08-20 10:31:28 +0200

    ADDED socket.io (cherry picked from commit 57c561e)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function (req, res) {
-    res.send('<h1>Hello world</hi>');
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function () {
