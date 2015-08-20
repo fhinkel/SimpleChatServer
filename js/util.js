562ca6d26429ed5d2a117015f7eccd401dd4b640
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-20 09:20:14 +0200

    REFACTOR Change greeting

--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
 exports.helloWorld = function() {
-    return 'Hello World, wie geht es euch?\n';
+    return 'Hello World, wie geht es euch?\n ' +
+        '<h1>Wir lassen TravisCI für uns arbeiten</h1>';
 };
