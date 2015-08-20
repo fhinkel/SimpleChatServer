124e75598fa6cb41e6ef03337b4f29e642a4d25c
nadjab <nadja.bogdanowa@gmail.com>
2015-08-20 15:38:45 +0200

    ADDED Variable auslesen und anzeigen

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var chat = require('./routes/chat');
-
 
 var app = express();
 
@@ -26,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/chat', chat);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-var users = {
-  tj: {name: 'John', age: 23},
-  nadja: {name: 'Nadja', age: 27}
-}
-
-var chat = {
-  message1: {user: 'John', date: '20. August', message: 'Hello'},
-  message2: {user: 'Nadja', date: '19. August', message: 'Hello all'}
-}
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simple Chat Server', users:users, chat:chat });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
