2ec4fa46ee7c3674765e73b17a3edc57d49ef40e
sara235 <rpg-freak@gmx.net>
2015-08-20 12:00:19 +0200

    UPDATED to final tutorial result (cherry picked from commit 1859c82)

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Deliver index.html with client javascript code
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+
+//Create server socket for client connection
 io.on('connection', function(socket){
     console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnect');
-    });
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat messages', msg);
+    })
+
+    //socket.on('disconnect', function(){  //Create disconnect, if client refreshes/exits index.html
+    //    console.log('user disconnect');
+    //});
 });
 
 
