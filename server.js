914247961395698caf24e940d08d3cb844141712
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-20 15:34:01 +0200

    FIX Issue with port
    Also, only build with node 0.10

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ io.on('connection', function(socket){
 });
 
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = process.env.PORT || '3000';
 http.listen(port, function(){
     console.log('listening on port ' + port);
 });
