61139beb5029398610a1c60b2704ebc29f00e6a8
DianaBarth <dianabarth87@googlemail.com>
2015-09-13 18:20:34 +0200

    Diana

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var AlleOnlineUser =[];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,9 +17,26 @@ io.on('connection', function (socket) {
 
     socket.on('chat message', function (msg) {
         io.emit('chat messages', msg);
-        msg = "{'Message':'" + msg + "'}";
-        console.log('message: ' + msg);
-    });
+
+        UserUndChat = msg.split(" TRENNER ");
+        user = userUndChat[0];
+        chat = userUndChat[1];
+
+        console.log('user: ' + user);
+        console.log('chat' + chat);
+
+        for (var usernr = 0; usernr < AlleOnlineUser.length; usernr++) {
+            if (AlleOnlineUser[usernr] = user) {
+                userSchonDrin = true;
+            }
+        }
+        if (userSchonDrin = false)
+        {
+            AlleOnlineUser.push(user);
+        }
+        AlleOnlineUser.sort;
+        io.emnit('Alle User ',AlleOnlineUser);
+
 });
 
 var port = process.env.PORT || '3000';
